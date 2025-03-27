6addbea776cc053b63bb1c41a65ce82d89de63ff
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-03-28 04:20:02 +0530

    made the accept connection function

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	"fmt"
 	"net"
 	"sync"
 
 	"github.com/zeusnotfound04/nano-mail/internal/config"
 	"github.com/zeusnotfound04/nano-mail/internal/limiter"
+	"golang.org/x/tools/go/analysis/passes/defers"
 )
 
 type Server struct {
@@ -40,3 +42,53 @@ func NewServer(cfg *config.Config) *Server {
 }
 
 
+func (s *Server) Start() error {
+	addr := fmt.Sprintf("%s:%s", s.config.Host, s.config.Port)
+
+	var err error
+	s.listener , err = net.Listen("tcp" , addr)
+	if err != nil {
+		return fmt.Errorf("Failed to start SMTP server: %w" , err)
+	}
+
+	s.config.Logger.Info("SMTP server started" ,
+		"host" , s.config.Host,
+		"port" , s.config.Port,
+		"domain" , s.config.Domain)
+	
+	s.wg.Add(1)
+	go s.
+}
+
+
+func (s *Server) acceptConnection() {
+	defer s.wg.Done()
+
+	for {
+		select {
+		case <-s.shutdown:
+			return
+		default:
+			conn, err := s.listener.Accept()
+			if err != nil {
+				s.config.Logger.Error("Error  accepting connection" , "error" , err)
+				return
+			}
+
+			remoteIP, _, _ := net.SplitHostPort(conn.RemoteAddr().Network().String())
+			if !s.rateLimiter.Allow(remoteIP) {
+				s.config.Logger.Warn("Connection rate Limit exceeded", "ip" , remoteIP)
+				conn.Write([]byte("421 Too Many Connections from your IP\r\n"))
+				conn.Close()
+				continue
+			}
+
+			s.wg.Add(1)
+			go func(c net.Conn , ip string) {
+				defer s.wg.Done()
+				defer s.rateLimiter.Release(ip)
+				s.handleConnection(c)
+			}(conn , remoteIP)
+		}
+	}
+}
