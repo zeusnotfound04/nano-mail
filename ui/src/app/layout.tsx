30282a75d64f81baa0485682c95b9474078f8550
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-04-11 17:10:41 +0530

    ADDED SPEED INSIGHT 🔒

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono  } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
+        <SpeedInsights/>
         {children}
         <Footer />
       </body>
